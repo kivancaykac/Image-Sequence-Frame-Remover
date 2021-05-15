238d056363088cd88fd2000ab6124a893bdeb7a1
Kivanc Aykac <kivanc.aykac@mail.utoronto.ca>
2021-05-15 18:19:22 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Image Sequence Frame Remover
  Frame Remover For Image Sequence Export
+Frame remover for image sequence export. Keeps every unique frame depending on the fps.
---
+++ imageSequenceFrameRemover.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 15:14:55 2021
+
+@author: kivancaykac
+removes every 24th frame in an exported image sequence file
+"""
+import os
+
+images = int(input("how many still images want to get at the end? "))
+fps = int(input("what is the fps? "))
+directory_path = input("input directory path? ")
+
+file_naming = "frame-"
+ext = ".png"
+wanted_frame_count = 0
+
+for i in range(images*fps):
+    if i == fps*wanted_frame_count:
+        wanted_frame_count += 1
+    else:
+        os.remove(directory_path+file_naming+f"{i}".zfill(6)+ext)
