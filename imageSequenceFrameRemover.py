13c3bd7fe52f0a4973ea760cc70444385303bb1e
Kivanc Aykac <kivanc.aykac@mail.utoronto.ca>
2021-05-15 19:35:34 +0300

    zfill update

--- imageSequenceFrameRemover.py
+++ imageSequenceFrameRemover.py
@@ -11,8 +11,9 @@ removes every 24th frame in an exported image sequence file
 import os
 
 images = int(input("how many still images want to get at the end? "))
-fps = int(input("what is the fps? "))
-directory_path = input("input directory path? ")
+fps = int(input("what is the fps?\nanswer: "))
+directory_path = input("input directory path?\nanswer: ")
+zfill_size = int(input("what is zfill size?\n(eg: zfill=4==>0000)\nanswer: "))
 
 file_naming = "frame-"
 ext = ".png"
@@ -22,4 +23,4 @@ for i in range(images*fps):
     if i == fps*wanted_frame_count:
         wanted_frame_count += 1
     else:
-        os.remove(directory_path+file_naming+f"{i}".zfill(6)+ext)
+        os.remove(directory_path+file_naming+f"{i}".zfill(zfill_size)+ext)
